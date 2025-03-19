d5cd6640fef7a1368596c59a5889e3f27be01ae8
Wim Van Aerschot <wim.van.aerschot@gmail.com>
2025-03-19 21:39:05 +0100

    added three scene

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Swiper from 'swiper';
 import 'swiper/swiper-bundle.css';
 import { Navigation, Pagination } from 'swiper/modules';
 import { apiKey } from './secret.js';
+import * as THREE from 'three';
 
 new Swiper('.swiper', {
   modules: [Navigation, Pagination],
@@ -46,3 +47,30 @@ async function updateWeather() {
 }
 
 updateWeather();
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
